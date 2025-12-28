a8c3e615de3e33558e6f4b7ff482da845212333c
Md Mosharrof Hossain <mdmosharrofhossain455@gmail.com>
2025-12-28 23:05:16 +0600

    add new file dynamic memory alocation

---
+++ Mod-2/dynamic_exersize.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int *var=new int(500);
+    cout<<var<<endl;
+    cout<<*var<<endl;
+    cout<<var<<" ndabcn x"<<endl;
+
+
+    return 0;
+    delete var;
+    
+    
+    cout<<var<<" ndabcn x"<<endl;
+}
